e2541b267d2f04dbb6e7d6b01a1286a80a82c046
Heeseung Seo <hirenn@websync.co.kr>
2012-07-18 16:46:03 +0900

    initial commit

---
+++ fixraws.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+#
+# fixraws.rb
+# Author: Seorenn
+#
+# Extract content name from file. 
+# And rename that to content name.
+#
+# Ex. [Sample-Raws] This is Test File - 01 (this 1234 123 test).txt
+#  -> This is Test File - 01.txt
+#
+
+require 'fileutils'
+
+def convertname(name)
+    r = /\[.*-Raws\]\s+(.*)\s+\(.*\)(\..+)/i
+    match = name.match r
+    if match
+        return $1.strip + $2.strip
+    end
+end
+
+# Remove excepted names from array
+def clearext(a)
+    a.reject! { |item| item =~ /.*\.part/i }
+    a
+end
+
+def discover
+    files = clearext Dir.entries "./"
+    files.delete(".")
+    files.delete("..")
+
+    files.each do |filename|
+        newname = convertname filename
+        next unless newname
+        puts "#{filename}\n\t-> #{newname}"
+        FileUtils.mv filename, newname
+    end
+end
+
+discover
